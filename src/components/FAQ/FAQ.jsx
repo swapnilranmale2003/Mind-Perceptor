de7d78ff238bf9e8acceba7e0c59b3782c7fdd5a
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-16 23:42:51 +0530

    working!

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaQuestion } from "react-icons/fa";
-import './FAQ.css'
+import "./FAQ.css";
 
 function FAQ() {
   const [expanded, setExpanded] = useState({});
@@ -17,22 +17,18 @@ function FAQ() {
   };
 
   return (
-    <div id="faq-section">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          height: "400px",
-        }}
-      >
+    <div id="faq-section" className="faq-section">
+      <div className="faq-header">
         <img
           src="https://images01.nicepage.com/a1389d7bc73adea1e1c1fb7e/f894db7507df5208a83a343c/Untitled-1.png"
           alt=""
+          className="faq-header-image"
         />
       </div>
       <div className="faqmain">
         <div className="faq" onClick={() => toggleQuestion(0)}>
-          <p className="question">Which tools and technologies do you use</p>
+          <p className="question">Which tools and technologies do you use?</p>
+          
         </div>
         {expanded[0] && (
           <div className="answer">
@@ -40,7 +36,10 @@ function FAQ() {
           </div>
         )}
         <div className="faq" onClick={() => toggleQuestion(1)}>
-          <p className="question">Will I be getting any support after completion of project?</p>
+          <p className="question">
+            Will I be getting any support after completion of the project?
+          </p>
+        
         </div>
         {expanded[1] && (
           <div className="answer">
@@ -48,7 +47,10 @@ function FAQ() {
           </div>
         )}
         <div className="faq" onClick={() => toggleQuestion(2)}>
-          <p className="question">What is the difference between native app and a web app?</p>
+          <p className="question">
+            What is the difference between a native app and a web app?
+          </p>
+     
         </div>
         {expanded[2] && (
           <div className="answer">
