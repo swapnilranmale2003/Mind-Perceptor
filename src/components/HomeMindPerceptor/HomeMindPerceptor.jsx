3145a467cce9972f5d742bccb63439782adaefb0
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-16 20:31:06 +0530

    working on homepage!

--- src/components/HomeMindPerceptor/HomeMindPerceptor.jsx
+++ src/components/HomeMindPerceptor/HomeMindPerceptor.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import './HomeMindPerceptor.css'
+import React from "react";
+import "./HomeMindPerceptor.css";
+import { Button } from "@mui/material";
+import { FaHandshakeAngle } from "react-icons/fa6";
+
 function HomeMindPerceptor() {
   return (
     <>
-   <div className="container">
-    <div className="row">
-        <div className="col-md-4">
-            H1
-        </div>
-        <div className="col-md-8">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-5">
+            <h1 className="h1-align">
+              Your IT partner to {"  "}
+              <span>
+                 <FaHandshakeAngle color="#007bff" size={"4rem"} />
+              </span>
+              {"  "}
+              reimagine lives
+            </h1>
+            <p>
+              Delivering best-in-class software solutions by leveraging
+              cutting-edge technology.
+            </p>
+            <Button size="large" variant="contained">
+              Contact Us
+            </Button>
+          </div>
+          <div className="col-md-7">
             <img src="assets/homeMP.jpg" alt="" />
+          </div>
         </div>
-    </div>
-   </div>
-   </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default HomeMindPerceptor
+export default HomeMindPerceptor;
